19870732c15f5e49cb24ff8a5a123dda81d898b7
kim kim <kim.ly2704@gmail.com>
2022-08-02 11:20:07 -0400

    Initial Commit

---
+++ ReadMe/ReadMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadMeApp.swift
+//  ReadMe
+//
+//  Created by kim ly on 2022-08-02.
+//
+
+import SwiftUI
+
+@main
+struct ReadMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
